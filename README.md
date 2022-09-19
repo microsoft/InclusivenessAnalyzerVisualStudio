67f16a1ad9df5e92bade1fe93d8152ea63534f1d
Merill Fernando <merill@merill.net>
2022-09-19 15:00:18 +1000

    Migrated from personal repo

--- README.md
+++ README.md
@@ -1,14 +1,48 @@
-# Project
+# Inclusiveness Analyzer
 
-> This repo has been populated by an initial template to help get you started. Please
-> make sure to update the content to build a great experience for community-building.
+> Make your code inclusive!
 
-As the maintainer of this project, please make a few updates:
+The Inclusiveness Analyzer is a Visual Studio extension that checks code for offensive / exclusive terms.
 
-- Improving this README.MD file to provide a great experience
-- Updating SUPPORT.MD with content about this project's support experience
-- Understanding the security reporting process in SECURITY.MD
-- Remove this section from the README
+It also provides context on why a word is exclusive and suggests alternate terms that can be used instead.
+
+![Intro clip](docs/assets/intro.gif)
+
+## About the project
+
+As humans, we hold many unconscious and implicit biases that we rely on to react quickly to our environment and any novel stimuli. However, since the unconscious brain processes and reacts with speed, we sometimes speak quickly without thinking, which may cause us to slip offensive terms and stereotypes although we mean no malice. 
+
+In order to confront these biases that we see in ourselves and others, we must rewire ourselves to regularly use inclusive practices (such as the words we speak). If you don't intentionally and proactively include, you will unintentionally exclude.
+
+> Join our effort to push out exclusive terms and make inclusive terms a part of our everyday vocabulary!
+
+Help us confront these biases by pushing out exclusive terms and making inclusive terms a part of our everyday vocabulary!
+
+## Installing via the NuGet Package
+
+The Inclusiveness Analyzer can be added to any C# project. Just install using NuGet and start writing code. The extension will be automatically loaded in Visual Studio for anyone that opens your project. Using the NuGet package is best when you are working on a project with a team.
+
+[Get from NuGet](https://www.nuget.org/packages/InclusivenessAnalyzer/)
+
+```powershell
+    Install-Package InclusivenessAnalyzer
+```
+
+> Important Note: The Inclusiveness Analyzer is only used during development time and does not affect your projects outputs or binaries.
+
+## Installing as a Visual Studio Extension
+
+If you would like the Inclusiveness Analyzer to run on any Visual Studio project you can install the extension directly into Visual Studio.
+
+[Get from the Visual Studio Marketplace](https://marketplace.visualstudio.com/items?itemName=InclusivenessAnalyzer.inclusivenessanalyzer)
+
+```
+Extensions > Manage Extensions > Search for 'Inclusiveness'
+```
+
+Happy inclusive :heart: coding!
+
+Icons made by [Freepik](https://www.flaticon.com/authors/freepik) from [www.flaticon.com](https://www.flaticon.com/)
 
 ## Contributing
 
---
+++ src/InclusivenessAnalyzer/InclusivenessAnalyzer.Test/InclusivenessAnalyzerUnitTests.cs
@@ -0,0 +1,183 @@
+﻿using Microsoft.CodeAnalysis.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using VerifyCS = InclusivenessAnalyzer.Test.CSharpCodeFixVerifier<
+    InclusivenessAnalyzer.InclusivenessAnalyzer,
+    InclusivenessAnalyzer.InclusivenessAnalyzerCodeFixProvider>;
+
+namespace InclusivenessAnalyzer.Test
+{
+    [TestClass]
+    public class InclusivenessAnalyzerUnitTest
+    {
+
+        //No diagnostics expected to show up
+        [TestMethod]
+        public async Task EmptyTest()
+        {
+            var test = @"";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [TestMethod]
+        public async Task ShortWordTest()
+        {
+            var test = @"class check {}";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [TestMethod]
+        public async Task ShortWordHeTest()
+        {
+            var test = @"class he {}";
+
+            var expected = VerifyCS.Diagnostic(InclusivenessAnalyzer.DiagnosticId).WithLocation(1, 7).WithArguments("he", "they");
+            await VerifyCS.VerifyAnalyzerAsync(test, expected);
+        }
+
+        //Diagnostic and CodeFix both triggered and checked for
+        [TestMethod]
+        public async Task WhitelistTypeNameTest()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class WhiteList
+        {   
+        }
+    }";  
+
+            var expected = VerifyCS.Diagnostic(InclusivenessAnalyzer.DiagnosticId).WithLocation(11,15).WithArguments("WhiteList", "allow list, access list, permit");
+            await VerifyCS.VerifyAnalyzerAsync(test, expected);
+        }
+
+        [TestMethod]
+        public async Task WhitelistMethodNameTest()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class Program
+        {   
+            public void WhiteList() { }
+        }
+    }";
+
+            var expected = VerifyCS.Diagnostic(InclusivenessAnalyzer.DiagnosticId).WithLocation(13, 25).WithArguments("WhiteList", "allow list, access list, permit");
+            await VerifyCS.VerifyAnalyzerAsync(test, expected);
+        }
+
+        [TestMethod]
+        public async Task WhitelistParameterTest()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class Program
+        {   
+            public void DoWork(string whiteList) { }
+        }
+    }";
+
+            var expected = VerifyCS.Diagnostic(InclusivenessAnalyzer.DiagnosticId).WithLocation(13, 39).WithArguments("whiteList", "allow list, access list, permit");
+            await VerifyCS.VerifyAnalyzerAsync(test, expected);
+        }
+
+        [TestMethod]
+        public async Task BlacklistPropertyTest()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class Program
+        {   
+            public int BlacklistNumber { get; set; }
+        }
+    }";
+
+            var expected1 = VerifyCS.Diagnostic(InclusivenessAnalyzer.DiagnosticId).WithLocation(13, 24).WithArguments("BlacklistNumber", "deny list, blocklist, exclude list");
+            var expected2 = VerifyCS.Diagnostic(InclusivenessAnalyzer.DiagnosticId).WithLocation(13, 42).WithArguments("get_BlacklistNumber", "deny list, blocklist, exclude list");
+            var expected3 = VerifyCS.Diagnostic(InclusivenessAnalyzer.DiagnosticId).WithLocation(13, 47).WithArguments("set_BlacklistNumber", "deny list, blocklist, exclude list");
+            await VerifyCS.VerifyAnalyzerAsync(test, expected1, expected2, expected3);
+        }
+
+        [TestMethod]
+        public async Task BlacklistFieldTest()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class Program
+        {   
+            public string BlackListValue;
+        }
+    }";
+
+            var expected = VerifyCS.Diagnostic("Inclusive").WithLocation(13, 27).WithArguments("BlackListValue", "deny list, blocklist, exclude list");
+            await VerifyCS.VerifyAnalyzerAsync(test, expected);
+        }
+
+        [TestMethod]
+        public async Task BlacklistCommentTest()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        /// <summary>
+        /// blacklist here
+        /// </summary>
+        class Program
+        {   
+            public string ListValue;
+        }
+    }";
+
+            var expected = VerifyCS.Diagnostic(InclusivenessAnalyzer.DiagnosticId).WithLocation(11, 12).WithArguments("blacklist", "deny list, blocklist, exclude list");
+            await VerifyCS.VerifyAnalyzerAsync(test, expected);
+        }
+    }
+}
